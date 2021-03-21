53dd23d324eb502343debaa78d73e64926351d98
chiayutsai <37895929+chiayutsai@users.noreply.github.com>
2021-03-21 16:15:09 +0800

    first commit

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,47 +1,26 @@
-let data = [
-  {
-    "id": 0,
-    "name": "肥宅心碎賞櫻3日",
-    "imgUrl": "https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80",
-    "area": "高雄",
-    "description": "賞櫻花最佳去處。肥宅不得不去的超讚景點！",
-    "group": 87,
-    "price": 1400,
-    "rate": 10
-  },
-  {
-    "id": 1,
-    "name": "貓空纜車雙程票",
-    "imgUrl": "https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-    "area": "台北",
-    "description": "乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",
-    "group": 99,
-    "price": 240,
-    "rate": 2
-  },
-  {
-    "id": 2,
-    "name": "台中谷關溫泉會1日",
-    "imgUrl": "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-    "area": "台中",
-    "description": "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
-    "group": 20,
-    "price": 1765,
-    "rate": 7
-  }
-];
+let data = [];
 let dataStr = ''
 let dataLength = data.length;
 let index = dataLength - 1;
-let ticketName = document.querySelector('#name');
-let ticketImgUrl = document.querySelector('#imgurl');
-let ticketArea = document.querySelector('#area');
-let ticketPrice = document.querySelector('#price');
-let ticketNumber = document.querySelector('#number');
-let ticketScore = document.querySelector('#score');
-let ticketDescription = document.querySelector('#description');
+const ticketName = document.querySelector('#name');
+const ticketImgUrl = document.querySelector('#imgurl');
+const ticketArea = document.querySelector('#area');
+const ticketPrice = document.querySelector('#price');
+const ticketNumber = document.querySelector('#number');
+const ticketScore = document.querySelector('#score');
+const ticketDescription = document.querySelector('#description');
 let state = 'success';
 let submit = document.querySelector('.submit');
+
+
+axios.get('https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json')
+  .then(function (response) {
+    data = response.data.data;
+    showData();
+  })
+
+
+
 
 
 
@@ -179,6 +158,12 @@ ticketName.addEventListener('keyup', function () {
 ticketImgUrl.addEventListener('change', function () {
   quickValidation(ticketImgUrl);
 });
+ticketPrice.addEventListener('change', function () {
+  quickValidation(ticketPrice);
+});
+ticketNumber.addEventListener('change', function () {
+  quickValidation(ticketNumber);
+});
 
 ticketScore.addEventListener('change', function () {
   rangeValidation(ticketScore);
@@ -228,4 +213,3 @@ $(document).ready(function () {
 
 
 
-showData();
